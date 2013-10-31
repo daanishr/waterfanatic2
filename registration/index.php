5847b04d4b0b788047431669fa994cdfd3199a6b
Vera Johne <vera@gangeskar.com>
2013-10-31 16:51:54 +0000

    edited index.php error fix6

--- registration/index.php
+++ registration/index.php
@@ -80,7 +80,7 @@
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
             echo "<td>".$registrant['date']."</td>";
-            echo "<td>".$registrants['company_name']."</td></tr>";
+            echo "<td>".$registrant['company_name']."</td></tr>";
         }
         echo "</table>";
     } else {
