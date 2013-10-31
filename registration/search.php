f870186947f8bcdf7d041b9e1f60f378d6ffef85
Vera Johne <vera@gangeskar.com>
2013-10-31 17:25:06 +0000

    edited search.php

--- registration/search.php
+++ registration/search.php
@@ -4,8 +4,14 @@
 </head>
 <body>
 	helloooo
-<!--
-<?php
+	
+	<form method="post" action="index.php" enctype="multipart/form-data" >
+      Search word <input type="text" name="search_id" id="search_id"/></br>
+    
+      <input type="search" name="search" value="search" />
+	</form>
+	
+	<?php
     // DB connection info
     //TODO: Update the values for $host, $user, $pwd, and $db
     //using the values you retrieved earlier from the portal.
@@ -21,19 +27,31 @@
     catch(Exception $e){
         die(var_dump($e));
     }
+    
+    //pattern match with search_id
+    /*
+    select * from registration_dbl where tags LIKE %search_id%;
+    
+    
+    **/
+    
+    
+    /*
     // Insert registration info
     if(!empty($_POST)) {
     try {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $date = date("Y-m-d");
+        $company_name = $_POST['company_name'];
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)";
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date, company_name) 
+                   VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
         $stmt->bindValue(3, $date);
+        $stmt->bindValue(4, $company_name);
         $stmt->execute();
     }
     catch(Exception $e) {
@@ -41,26 +59,29 @@
     }
     echo "<h3>Your're registered!</h3>";
     }
+    **/
     // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl";
+    $sql_select = "SELECT * FROM registration_tbl WHERE 'name' LIKE '%search_id%'";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll(); 
     if(count($registrants) > 0) {
-        echo "<h2>People who are registered:</h2>";
+        echo "<h2>Search results:</h2>";
         echo "<table>";
         echo "<tr><th>Name</th>";
         echo "<th>Email</th>";
-        echo "<th>Date</th></tr>";
+        echo "<th>Date</th>";
+        echo "<th>company_name</th></tr>";
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['date']."</td></tr>";
+            echo "<td>".$registrant['date']."</td>";
+            echo "<td>".$registrant['company_name']."</td></tr>";
         }
         echo "</table>";
     } else {
-        echo "<h3>No one is currently registered.</h3>";
+        echo "<h3>No result</h3>";
     }
 ?>
---//>
+	
 </body>
 </html>
