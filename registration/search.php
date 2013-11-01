f204f3fd5e987392cbf88737b737831d4bbfabd8
Vera Johne <vera@gangeskar.com>
2013-11-01 12:35:05 +0000

    edited search.php

--- registration/search.php
+++ registration/search.php
@@ -1,12 +1,10 @@
 <html>
 <head>
-	
+	<title> Search </title>	
 </head>
 <body>
-	helloooo
-	
 	<form method="post" action="search.php" enctype="multipart/form-data" >
-      Search word <input type="text" name="search_idd" id="search_idd"/></br>
+      Search word <input type="text" name="search" id="search_id"/></br>
       <input type="submit" name="submit" value="search" />
 	</form>
 	
@@ -14,8 +12,6 @@
 	<?php
 	
     // DB connection info
-    //TODO: Update the values for $host, $user, $pwd, and $db
-    //using the values you retrieved earlier from the portal.
     $host = "eu-cdbr-azure-west-b.cloudapp.net";
     $user = "b6ddb7ebfbbe78";
     $pwd = "2fbf45c4";
@@ -30,38 +26,37 @@
     }
      
     //get search word
-  
-    
     if(!empty($_POST)) {
-    try {
-    	$search_id = $_POST['search_id'];
-    }
-    catch(Exception $e) {
-        die(var_dump($e));
-    }
-    
-    
-    // Retrieve data
-    
-    $sql_select = "SELECT * FROM registration_tbl WHERE 'name' LIKE '%search_id%'";
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll(); 
-    if(count($registrants) > 0) {
-        echo "<h2>Search results:</h2>";
-        echo "<table>";
-        echo "<tr><th>Name</th>";
-        echo "<th>Email</th>";
-        echo "<th>Date</th>";
-        echo "<th>company_name</th></tr>";
-        foreach($registrants as $registrant) {
-            echo "<tr><td>".$registrant['name']."</td>";
-            echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['date']."</td>";
-            echo "<td>".$registrant['company_name']."</td></tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "<h3>No result</h3>";
+		try {
+			$search_id = $_POST['search'];
+		}
+		catch(Exception $e) {
+			die(var_dump($e));
+		}
+	
+	
+		// Retrieve data
+	
+		$sql_select = "SELECT * FROM registration_tbl WHERE name LIKE '%$search_id%'";
+		$stmt = $conn->query($sql_select);
+		$registrants = $stmt->fetchAll(); 
+		if(count($registrants) > 0) {
+			echo "<h2>Search results:</h2>";
+			echo "<table>";
+			echo "<tr><th>Name</th>";
+			echo "<th>Email</th>";
+			echo "<th>Date</th>";
+			echo "<th>company_name</th></tr>";
+			foreach($registrants as $registrant) {
+				echo "<tr><td>".$registrant['name']."</td>";
+				echo "<td>".$registrant['email']."</td>";
+				echo "<td>".$registrant['date']."</td>";
+				echo "<td>".$registrant['company_name']."</td></tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "<h3>No result</h3>";
+		}
     }
    
 ?>
